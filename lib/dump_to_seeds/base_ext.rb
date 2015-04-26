6917ad7f48f16f5a2a878ab01bfe6d5c5c21fb47
Dmitri Chromov <chromov@gmail.com>
2015-04-26 19:19:47 +0300

    Recursive relations and save_id option

--- lib/dump_to_seeds/base_ext.rb
+++ lib/dump_to_seeds/base_ext.rb
@@ -1,7 +1,9 @@
 module DumpToSeeds::BaseExt
 
-  def to_seed(includes = [])
-    if includes.blank?
+  def to_seed(includes = [], opts = {})
+    @dump_options = {:save_id => false}.merge(opts)
+
+    if includes.blank? && !@dump_options[:save_id]
       build_simple_rb(self)
     else
       dump_object(self, includes)
@@ -16,21 +18,39 @@ module DumpToSeeds::BaseExt
 
     dump_arr = []
     var_name = obj.class.name.underscore
-    dump_arr << "#{var_name} = #{build_simple_rb(obj, !(parent_var && rel_key))}"
+    var_name += "_#{obj.id}"
 
-    if(parent_var && rel_key)
-      dump_arr << "#{var_name}.#{rel_key} = #{parent_var}.id"
+    dump_arr << "#{var_name} = #{build_simple_rb(obj, (!(parent_var && rel_key) && !@dump_options[:save_id]))}"
+
+    if((parent_var && rel_key) || @dump_options[:save_id])
+      if(@dump_options[:save_id])
+        dump_arr << "#{var_name}.id = #{obj.id}"
+      end
+      if(parent_var && rel_key)
+        dump_arr << "#{var_name}.#{rel_key} = #{parent_var}.id"
+      end
       dump_arr << "#{var_name}.save"
     end
 
     includes.each do |i|
       rel, next_ass = i.is_a?(Hash) ? i.to_a.first : [i, []]
 
-      refl = obj.class.reflect_on_association(rel)
-      if refl
-        rel_objs = [obj.send(rel)].flatten.compact
-        rel_objs.each do |ro|
-          dump_arr << dump_object(ro, next_ass, var_name, refl.primary_key_name)
+      if rel.to_s =~ /!$/
+        m_name = rel.to_s.gsub(/!$/, '')
+        if obj.respond_to?(m_name)
+          rel_objs = [obj.send(m_name)].flatten.compact
+          rel_objs.each do |ro|
+            p_key = next_ass.to_s
+            dump_arr << dump_object(ro, includes, var_name, p_key)
+          end
+        end
+      else
+        refl = obj.class.reflect_on_association(rel)
+        if refl
+          rel_objs = [obj.send(rel)].flatten.compact
+          rel_objs.each do |ro|
+            dump_arr << dump_object(ro, next_ass, var_name, refl.primary_key_name)
+          end
         end
       end
     end
